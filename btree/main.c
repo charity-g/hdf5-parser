98b2fe347768611d412bc809bc30e5f227ae4949
cherr <charity.grey1@gmail.com>
2026-04-19 17:23:28 -0700

    wishlist tests

--- btree/main.c
+++ btree/main.c
@@ -10,9 +10,29 @@ void initialization() {
     destruct_btree(btree);
 }
 
+
+void test_insert() {
+}
+
+void test_insert_many() {
+}
+
+void test_search() {
+}
+
+void test_delete() {
+}
+
+void test_delete_many() {
+}
+
 int main(int argc, char *argv[])
 {
-    
     initialization();
+    test_insert();
+    test_insert_many();
+    test_search();
+    test_delete();
+    test_delete_many();
     return EXIT_SUCCESS;
 }
