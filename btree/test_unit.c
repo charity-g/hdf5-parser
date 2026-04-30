41d9facd0124e6aa083d113c5a55275f37f47add
cherr <charity.grey1@gmail.com>
2026-04-29 22:39:48 -0700

    rename function to be more aligned

--- btree/test_unit.c
+++ btree/test_unit.c
@@ -3,21 +3,21 @@
 // #include "b_tree.c"
 
 
-int get_m_div_2(int m) {
+int get_min_node_capacity(int m) {
     return m % 2 == 0 ? m / 2 : (m / 2) + 1;
 }
 
-int test_get_m_div_2() {
+int test_get_min_node_capacity() {
     int errors = 0;
 
-    int res1 = get_m_div_2(8);
+    int res1 = get_min_node_capacity(8);
     int exp1 = 4;
-    if (res1 != exp1) printf("test_get_m_div_2 failed: res1. expected: %d actual: %d", exp1, res1);
+    if (res1 != exp1) printf("test_get_min_node_capacity failed: res1. expected: %d actual: %d", exp1, res1);
     
 
-    int res2 = get_m_div_2(7);
+    int res2 = get_min_node_capacity(7);
     int exp2 = 4;
-    if (res2 != exp2) printf("test_get_m_div_2 failed: res2. expected: %d actual: %d", exp2, res2);
+    if (res2 != exp2) printf("test_get_min_node_capacity failed: res2. expected: %d actual: %d", exp2, res2);
 
     if (errors > 0) return 1;
     return 0;
@@ -27,7 +27,7 @@ int test_get_m_div_2() {
 int main(int argc, char *argv[])
 {
     int errors = 0;
-    errors += test_get_m_div_2();
+    errors += test_get_min_node_capacity();
     
     if (errors > 0) {
         printf("failed");
