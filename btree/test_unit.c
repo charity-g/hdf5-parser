281e3e63cfd686b815f03245ca0eaca600bfcd63
cherr <charity.grey1@gmail.com>
2026-04-29 22:38:03 -0700

    add unit test for m

---
+++ btree/test_unit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+// #include "b_tree.c"
+
+
+int get_m_div_2(int m) {
+    return m % 2 == 0 ? m / 2 : (m / 2) + 1;
+}
+
+int test_get_m_div_2() {
+    int errors = 0;
+
+    int res1 = get_m_div_2(8);
+    int exp1 = 4;
+    if (res1 != exp1) printf("test_get_m_div_2 failed: res1. expected: %d actual: %d", exp1, res1);
+    
+
+    int res2 = get_m_div_2(7);
+    int exp2 = 4;
+    if (res2 != exp2) printf("test_get_m_div_2 failed: res2. expected: %d actual: %d", exp2, res2);
+
+    if (errors > 0) return 1;
+    return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    int errors = 0;
+    errors += test_get_m_div_2();
+    
+    if (errors > 0) {
+        printf("failed");
+    }
+    return EXIT_SUCCESS;
+}
